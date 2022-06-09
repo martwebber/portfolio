4502944f256b3269dca9d95c34e536b0f65e3b82
Martin Mwangi <mwangi.martin24@gmail.com>
2022-06-09 14:17:32 +0300

    add eventlisteners to social icons

--- js/index.js
+++ js/index.js
@@ -3,50 +3,31 @@ let linkedin = document.getElementById("linkedin-link");
 let twitter = document.getElementById("twitter-link");
 
 github.addEventListener("mouseenter", function (e) {
-    if (e.type === "mouseenter") {
-        console.log(`${e.target.id} mouseenter`)
-        github.style.backgroundColor = "green";
-        github.style.color = "#fff"
-    } 
+    e.target.style.width="40px"
+    e.target.style.height="40px"
   });
 
-github.addEventListener("mouseout", function (e) {
-    if (e.type === "mouseout") {
-        console.log(`${e.target.id} mouseout`)
-        github.style.backgroundColor = "";
-        github.style.color = "";
-    }
+  github.addEventListener("mouseout", function (e) {
+    e.target.style.width="30px"
+    e.target.style.height="30px"
   });
 
-linkedin.addEventListener("mouseenter", function (e) {
-
-    if (e.type === "mouseenter") {
-        console.log(`${e.target.id} mouseenter`)
-        linkedin.style.backgroundColor = "#0072b1";
-        linkedin.style.color = "#ffffff";
-    }
+  linkedin.addEventListener("mouseenter", function (e) {
+    e.target.style.width="40px"
+    e.target.style.height="40px"
   });
 
-linkedin.addEventListener("mouseout", function (e) {
-    if (e.type === "mouseout") {
-        console.log(`${e.target.id} mouseout`)
-        linkedin.style.backgroundColor = "";
-        linkedin.style.color = "";
-    }
+  linkedin.addEventListener("mouseout", function (e) {
+    e.target.style.width="30px"
+    e.target.style.height="30px"
   });
 
-twitter.addEventListener("mouseenter", function (e) {
-    if (e.type === "mouseenter") {
-        console.log(`${e.target.id} mouseout`)
-        twitter.style.backgroundColor = "#1da1f2";
-        twitter.style.color = "#fff";
-    } 
+  twitter.addEventListener("mouseenter", function (e) {
+    e.target.style.width="40px"
+    e.target.style.height="40px"
   });
 
-twitter.addEventListener("mouseout", function (e) {
-    if (e.type === "mouseout") {
-        console.log(`${e.target.id} mouseenter`)
-        twitter.style.backgroundColor = "";
-        twitter.style.color = "";
-    }
+  twitter.addEventListener("mouseout", function (e) {
+    e.target.style.width="30px"
+    e.target.style.height="30px"
   });
